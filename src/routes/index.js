71bd926415d509661a7bdfc56f75bfb65e8bdadf
Valentin Omnès <valentin.omnes@gmail.com>
2022-05-05 12:42:28 +0200

    alternate username

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,22 +2,53 @@ const express = require('express');
 const router = express.Router();
 var os = require('os');
 var ip = require('ip');
-const today = new Date();
+var https = require('https');
+const { response } = require('express');
 
+const today = new Date();
 const year = today.getFullYear(); 
+var extPublicIP = '';
+var currentOS = process.platform;
+var processUserName = process.env.USER;
+
+
+
+var callback = function(err, ip){
+  if(err){
+      return console.log(err);
+  }
+  console.log('Our public IP is', ip);
+  // do something here with the IP address
+};
+
+https.get({
+  host: 'api.ipify.org',
+}, function(response) {
+  extPublicIP = '';
+  response.on('data', function(d) {
+    extPublicIP += d;
+  });
+  // response.on('end', function() {
+  //     if(ip){
+  //         callback(null, ip);
+  //     } else {
+  //         callback('could not get public ip address :(');
+  //     }
+  // });
+});
 router.get('/', (req, res) => {
   var publicIP = req.headers['x-forwarded-for'] || req.remoteAddress || req.socket.remoteAddress || req.socket.remoteAddress;
   const userInfo = os.userInfo();
   const username = userInfo.username;
-
   var localIP = ip.address();
-  res.render('index', { title: 'First Web Node', publicIP: publicIP, username: username, localIP: localIP ,currentYear: year });
   
+  res.render('index', { title: 'First Web Node', publicIP: publicIP, username: username,alternateUserName: processUserName ,localIP: localIP ,currentYear: year , publicAPIIP: extPublicIP, currentOS: currentOS});
 });
 
 router.get('/contact', (req, res) => { 
 
   res.render('contact', { title: 'Contact Page',currentYear: year });
 });
+
 
 module.exports = router;
